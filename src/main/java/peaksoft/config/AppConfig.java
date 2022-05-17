304eadb1a7652b63174687ad7ed78715b7a57ba0
elnurakaparova1 <elnura.kaparova99@gmail.com>
2022-05-18 04:21:32 +0600

    done

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -3,15 +3,55 @@ package peaksoft.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import peaksoft.model.Island2;
-import peaksoft.model.Wood3;
+import peaksoft.KoscheiTheDeathless;
+import peaksoft.model.*;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
 
+   @Bean
+   public static KoscheiTheDeathless koscheiTheDeathless(){
+       return new KoscheiTheDeathless();
+   }
     @Bean
-    public static Island2 getIsland(Wood3 wood) {
+    public Ocean1 ocean1(){
+        return new Ocean1();
+    }
+
+    @Bean
+    public static Island2 island2(Wood3 wood){
         return new Island2(wood);
     }
+
+    @Bean
+    public static Wood3 wood3(Rabbit4 rabbit4){
+        return new Wood3(rabbit4);
+    }
+
+    @Bean
+    public static Rabbit4 rabbit4(Duck5 duck5){
+        return new Rabbit4(duck5);
+    }
+
+    @Bean
+    public static Duck5 duck5(Egg6 egg6){
+        return new Duck5(egg6);
+    }
+
+    @Bean
+    public static Egg6 egg6(Needle7 needle7){
+        return new Egg6(needle7);
+    }
+
+    @Bean
+    public static Needle7 needle7() {
+        return new Needle7();
+    }
+
+    @Bean
+    public static Deth8 deth8(){
+       return new Deth8();
+    }
+
 }
--- src/main/java/peaksoft/model/Needle7.java
+++ src/main/java/peaksoft/model/Needle7.java
@@ -1,9 +1,18 @@
 package peaksoft.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Needle7 {
+
+
+
+    @Autowired
+    public Needle7() {
+
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( ";
     }
 }
